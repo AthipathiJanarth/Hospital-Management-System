78a97e95650167d098d117797b04d162c103206a
Athipathi Janarth <113406747+Athipathi-Janarth@users.noreply.github.com>
2022-09-27 12:30:00 -0400

    Created Base Setup & Models

---
+++ EmployeeRegistry/src/main/java/models/employeeDetails.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+import java.awt.Image;
+import java.util.Date;
+
+/**
+ *
+ * @author athipathi
+ */
+public class employeeDetails {
+    private String Name;
+    private String employeeID;
+    private int age;
+    private String gender;
+    private Date startDate;
+    private int level;
+    private String teamInfo;
+    private String positionTitle;
+    private Image profilePhoto; 
+    private contactInfo contact;
+    
+}
---
+++ EmployeeRegistry/src/main/java/models/employeeDirectory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author athipathi
+ */
+public class employeeDirectory {
+    
+    private ArrayList<employeeDetails> employeeList;
+}
